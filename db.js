2174ef76714f94563392cda3d28f20b2f3a46b55
Supawit-jt-23 <s6802042857108@email.kmutnb.ac.th>
2026-03-24 23:19:30 +0700

    backend method

---
+++ db.js
@@ -0,0 +1,19 @@
+import { createConnection } from "mysql2";
+
+const db = createConnection({
+  host: "localhost",
+  port : 3306,
+  user: "root",
+  password: "",
+  database: "coffee_shop",
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error("เชื่อมต่อฐานข้อมูลไม่สำเร็จ:", err.message);
+    process.exit(1);
+  }
+  console.log("เชื่อมต่อฐานข้อมูลสำเร็จ");
+});
+
+export default db;
